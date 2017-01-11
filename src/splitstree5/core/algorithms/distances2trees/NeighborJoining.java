c9881ce49135995ba4702a4e35eb83f31cbf6258
Daria <daria.elena.evseeva@gmail.com>
2017-01-11 21:54:21 +0100

    working on NJ

--- src/splitstree5/core/algorithms/distances2trees/NeighborJoining.java
+++ src/splitstree5/core/algorithms/distances2trees/NeighborJoining.java
@@ -23,6 +23,7 @@ package splitstree5.core.algorithms.distances2trees;
 import jloda.graph.Edge;
 import jloda.graph.Node;
 import jloda.phylo.PhyloTree;
+import jloda.util.Basic;
 import jloda.util.CanceledException;
 import jloda.util.ProgressListener;
 import splitstree5.core.algorithms.Algorithm;
@@ -65,117 +66,145 @@ public class NeighborJoining extends Algorithm<DistancesBlock, TreesBlock> {
         HashMap<String, Node> Taxa2Nodes = new HashMap<>();
         StringBuffer TaxaLabels[] = new StringBuffer[nTax + 1];
 
-        initialize(nTax, taxaBlock, tree, Taxa2Nodes, TaxaLabels);
+        try {
 
-        double[][] distanceMatrix = fillDistanceMatrix(nTax, distances);
-        double[] divergences = computeDivergences(distanceMatrix);
+            initialize(nTax, taxaBlock, tree, Taxa2Nodes, TaxaLabels);
+
+            //double[][] distanceMatrix = fillDistanceMatrix(nTax, distances);
+            //double[] divergences = computeDivergences(distanceMatrix);
 
 
-        //double minDist = Double.MAX_VALUE;
-        int i_min = 0, j_min = 0;
-        double temp, dist2i, dist2j;  //new edge weights
-        StringBuffer mergedTaxa_i; //labels of taxa that are being merged
-        StringBuffer mergedTaxa_j;
-        Node newParentNode;
-        Edge edge2i, edge2j; //from tax_old to new=merged edge
+            //double minDist = Double.MAX_VALUE;
+            int i_min = 0, j_min = 0;
+            double temp, dist2i, dist2j;  //new edge weights
+            StringBuffer mergedTaxa_i; //labels of taxa that are being merged
+            StringBuffer mergedTaxa_j;
+            Node newParentNode;
+            Edge edge2i, edge2j; //from tax_old to new=merged edge
 
-        // actual for (finding all nearest Neighbors)
-        for (int actual = nTax; actual > 2; actual--) {
-            // find: min D (h, b, b)
-            double minDist = Double.MAX_VALUE;
-            for (int i = 1; i < nTax; i++) {
-                if (distanceMatrix[0][i] == 0.0) continue;
-                for (int j = i + 1; j <= nTax; j++) {
-                    if (distanceMatrix[0][j] == 0.0)
+            double[][] distanceMatrix = new double[nTax + 1][nTax + 1];
+            double[] divergences = new double[nTax + 1];
+            for (int i = 0; i <= nTax; i++) {
+                distanceMatrix[0][i] = 1.0; // with 1.0 marked columns indicate columns/rows
+                distanceMatrix[i][0] = 1.0;// that haven't been deleted after merging
+            }
+            for (int i = 1; i <= nTax; i++) {
+                for (int j = 1; j <= nTax; j++) { //fill up the
+                    if (i < j)
+                        distanceMatrix[i][j] = distances.get(i, j);// distance matix h
+                    else
+                        distanceMatrix[i][j] = distances.get(j, i);
+                }
+            }
+
+            // calculate b:
+            for (int i = 1; i <= nTax; i++) {
+                for (int j = 1; j <= nTax; j++) {
+                    divergences[i] += distanceMatrix[i][j];
+                }
+            }
+
+            // actual for (finding all nearest Neighbors)
+            for (int actual = nTax; actual > 2; actual--) {
+                // find: min D (h, b, b)
+                double minDist = Double.MAX_VALUE;
+                for (int i = 1; i < nTax; i++) {
+                    if (distanceMatrix[0][i] == 0.0) continue;
+                    for (int j = i + 1; j <= nTax; j++) {
+                        if (distanceMatrix[0][j] == 0.0)
+                            continue;
+                        if (distanceMatrix[i][j] - ((divergences[i] + divergences[j]) / (actual - 2)) < minDist) {
+                            minDist = distanceMatrix[i][j] - ((divergences[i] + divergences[j]) / (actual - 2));
+                            i_min = i;
+                            j_min = j;
+                        }
+                    }
+                }
+                dist2i = 0.5 * (distanceMatrix[i_min][j_min] + divergences[i_min] / (actual - 2)
+                        - divergences[j_min] / (actual - 2));
+                dist2j = 0.5 * (distanceMatrix[i_min][j_min] + divergences[j_min] / (actual - 2)
+                        - divergences[i_min] / (actual - 2));
+
+                distanceMatrix[j_min][0] = 0.0;// marking
+                distanceMatrix[0][j_min] = 0.0;
+
+                // tax taxa update:
+                mergedTaxa_i = new StringBuffer(TaxaLabels[i_min].toString());
+                mergedTaxa_j = new StringBuffer(TaxaLabels[j_min].toString());
+                TaxaLabels[i_min].insert(0, "(");
+                TaxaLabels[i_min].append(",");
+                TaxaLabels[i_min].append(TaxaLabels[j_min]);
+                TaxaLabels[i_min].append(")");
+                TaxaLabels[j_min].delete(0, TaxaLabels[j_min].length());
+
+                // b update:
+
+                divergences[i_min] = 0.0;
+                divergences[j_min] = 0.0;
+
+                // fusion of h
+                // double h_min = h[i_min][j_min];
+
+                for (int i = 1; i <= nTax; i++) {
+                    if (distanceMatrix[0][i] == 0.0)
                         continue;
-                    if (distanceMatrix[i][j] - ((divergences[i] + divergences[j]) / (actual - 2)) < minDist) {
-                        minDist = distanceMatrix[i][j] - ((divergences[i] + divergences[j]) / (actual - 2));
-                        i_min = i;
-                        j_min = j;
+                    temp = (distanceMatrix[i][i_min] + distanceMatrix[i][j_min] - dist2i - dist2j) / 2; // correct NJ
+                    if (i != i_min) {
+                        divergences[i] = divergences[i] - distanceMatrix[i][i_min] - distanceMatrix[i][j_min] + temp;
+                    }
+                    divergences[i_min] += temp;
+                    distanceMatrix[i][i_min] = temp;
+                    divergences[j_min] = 0.0;
+                }
+
+                for (int i = 0; i <= nTax; i++) {
+                    distanceMatrix[i_min][i] = distanceMatrix[i][i_min];
+                    distanceMatrix[i][j_min] = 0.0;
+                    distanceMatrix[j_min][i] = 0.0;
+                }
+
+                // generate new Node for merged Taxa:
+                newParentNode = tree.newNode();
+                Taxa2Nodes.put(TaxaLabels[i_min].toString(), newParentNode);
+
+                // generate Edges from two Taxa that are merged to one:
+                edge2i = tree.newEdge(Taxa2Nodes.get(mergedTaxa_i.toString()), newParentNode);
+                tree.setWeight(edge2i, Math.max(dist2i, 0.0));
+                edge2j = tree.newEdge(Taxa2Nodes.get(mergedTaxa_j.toString()), newParentNode);
+                tree.setWeight(edge2j, Math.max(dist2j, 0.0));
+            }
+
+            //todo : evaluate last two nodes: save indexes?
+            // evaluating last two nodes:
+            for (int i = 1; i <= nTax; i++) {
+                if (distanceMatrix[0][i] == 1.0) {
+                    i_min = i;
+                    i++;
+
+                    for (; i <= nTax; i++) {
+                        if (distanceMatrix[0][i] == 1.0) {
+                            j_min = i;
+                        }
                     }
                 }
             }
-            dist2i = 0.5 * (distanceMatrix[i_min][j_min] + divergences[i_min] / (actual - 2)
-                    - divergences[j_min] / (actual - 2));
-            dist2j = 0.5 * (distanceMatrix[i_min][j_min] + divergences[j_min] / (actual - 2)
-                    - divergences[i_min] / (actual - 2));
-
-            distanceMatrix[j_min][0] = 0.0;// marking
-            distanceMatrix[0][j_min] = 0.0;
-
-            // tax taxa update:
             mergedTaxa_i = new StringBuffer(TaxaLabels[i_min].toString());
             mergedTaxa_j = new StringBuffer(TaxaLabels[j_min].toString());
+
             TaxaLabels[i_min].insert(0, "(");
             TaxaLabels[i_min].append(",");
             TaxaLabels[i_min].append(TaxaLabels[j_min]);
             TaxaLabels[i_min].append(")");
-            TaxaLabels[j_min].delete(0, TaxaLabels[j_min].length());
-
-            // b update:
-
-            divergences[i_min] = 0.0;
-            divergences[j_min] = 0.0;
-
-            // fusion of h
-            // double h_min = h[i_min][j_min];
-
-            for (int i = 1; i <= nTax; i++) {
-                if (distanceMatrix[0][i] == 0.0)
-                    continue;
-                temp = (distanceMatrix[i][i_min] + distanceMatrix[i][j_min] - dist2i - dist2j) / 2; // correct NJ
-                if (i != i_min) {
-                    divergences[i] = divergences[i] - distanceMatrix[i][i_min] - distanceMatrix[i][j_min] + temp;
-                }
-                divergences[i_min] += temp;
-                distanceMatrix[i][i_min] = temp;
-                divergences[j_min] = 0.0;
-            }
-
-            for (int i = 0; i <= nTax; i++) {
-                distanceMatrix[i_min][i] = distanceMatrix[i][i_min];
-                distanceMatrix[i][j_min] = 0.0;
-                distanceMatrix[j_min][i] = 0.0;
-            }
+            TaxaLabels[j_min].delete(0, TaxaLabels[j_min].length()); //not neces. but sets content to NULL
 
             // generate new Node for merged Taxa:
-            newParentNode = tree.newNode();
-            Taxa2Nodes.put(TaxaLabels[i_min].toString(), newParentNode);
-
             // generate Edges from two Taxa that are merged to one:
-            edge2i = tree.newEdge(Taxa2Nodes.get(mergedTaxa_i.toString()), newParentNode);
-            tree.setWeight(edge2i, Math.max(dist2i, 0.0));
-            edge2j = tree.newEdge(Taxa2Nodes.get(mergedTaxa_j.toString()), newParentNode);
-            tree.setWeight(edge2j, Math.max(dist2j, 0.0));
+            edge2i = tree.newEdge(Taxa2Nodes.get(mergedTaxa_i.toString()), Taxa2Nodes.get(mergedTaxa_j.toString()));
+            tree.setWeight(edge2i, Math.max(distanceMatrix[i_min][j_min], 0.0));
+        } catch (Exception ex) {
+            Basic.caught(ex);
+            System.err.println(ex);
         }
-
-        //todo : evaluate last two nodes: save indexes?
-        // evaluating last two nodes:
-        for (int i = 1; i <= nTax; i++) {
-            if (distanceMatrix[0][i] == 1.0) {
-                i_min = i;
-                i++;
-
-                for (; i <= nTax; i++) {
-                    if (distanceMatrix[0][i] == 1.0) {
-                        j_min = i;
-                    }
-                }
-            }
-        }
-        mergedTaxa_i = new StringBuffer(TaxaLabels[i_min].toString());
-        mergedTaxa_j = new StringBuffer(TaxaLabels[j_min].toString());
-
-        TaxaLabels[i_min].insert(0, "(");
-        TaxaLabels[i_min].append(",");
-        TaxaLabels[i_min].append(TaxaLabels[j_min]);
-        TaxaLabels[i_min].append(")");
-        TaxaLabels[j_min].delete(0, TaxaLabels[j_min].length()); //not neces. but sets content to NULL
-
-        // generate new Node for merged Taxa:
-        // generate Edges from two Taxa that are merged to one:
-        edge2i = tree.newEdge(Taxa2Nodes.get(mergedTaxa_i.toString()), Taxa2Nodes.get(mergedTaxa_j.toString()));
-        tree.setWeight(edge2i, Math.max(distanceMatrix[i_min][j_min], 0.0));
 
         return tree;
     }
@@ -260,11 +289,11 @@ public class NeighborJoining extends Algorithm<DistancesBlock, TreesBlock> {
         }
 
         double[][] dist =
-                {{0, 0, 0, 0, 0, 0},
-                {5, 0,	0, 0, 0, 0},
-                {4, 7, 0, 0, 0, 0},
-                {7, 10, 7, 0, 0, 0},
-                {6, 9, 6, 5, 0, 0},
+                {{0, 5, 4, 7, 6, 8},
+                {5, 0,	7, 10, 9, 11},
+                {4, 7, 0, 7, 6, 8},
+                {7, 10, 7, 0, 5, 9},
+                {6, 9, 6, 5, 0, 8},
                 {8, 11, 8, 9, 8, 0}};
 
         distancesBlock.set(dist);
--- src/splitstree5/core/algorithms/distances2trees/UPGMA.java
+++ src/splitstree5/core/algorithms/distances2trees/UPGMA.java
@@ -3,6 +3,7 @@ package splitstree5.core.algorithms.distances2trees;
 import jloda.graph.Edge;
 import jloda.graph.Node;
 import jloda.phylo.PhyloTree;
+import jloda.util.Basic;
 import jloda.util.CanceledException;
 import jloda.util.ProgressListener;
 import splitstree5.core.algorithms.Algorithm;
@@ -35,102 +36,106 @@ public class UPGMA extends Algorithm<DistancesBlock, TreesBlock> {
         Node[] subtrees = new Node[ntax + 1];
         int[] sizes = new int[ntax + 1];
         double[] heights = new double[ntax + 1];
-
-        for (int i = 1; i <= ntax; i++) {
-            subtrees[i] = tree.newNode();
-            tree.setLabel(subtrees[i], taxaBlock.getLabel(i));
-            sizes[i] = 1;
-        }
-
-        double d[][] = new double[ntax + 1][ntax + 1];// distance matix
-
-        //Initialise d
-        //Compute the closest values for each taxa.
-        for (int i = 1; i <= ntax; i++) {
-            for (int j = i + 1; j <= ntax; j++) {
-                //d[i][j] = d[j][i] = (distances.get(i, j) + distances.get(j, i)) / 2.0;
-                double sum = distances.get(i, j) + distances.get(j, i);
-                if(sum == distances.get(i, j) || sum == distances.get(j, i)){
-                    d[i][j] = d[j][i] = sum;
-                }else {
-                    d[i][j] = d[j][i] = sum / 2.0;
-                }
+        try {
+            for (int i = 1; i <= ntax; i++) {
+                subtrees[i] = tree.newNode();
+                tree.setLabel(subtrees[i], taxaBlock.getLabel(i));
+                sizes[i] = 1;
             }
-        }
 
-        int steps = 0;
-        for (int actual = ntax; actual > 2; actual--) {
+            double d[][] = new double[ntax + 1][ntax + 1];// distance matix
 
-            int i_min = 0, j_min = 0;
-            //Find closest pair.
-            double d_min = Double.MAX_VALUE;
-            for (int i = 1; i <= actual; i++) {
-                for (int j = i + 1; j <= actual; j++) {
-                    double dij = d[i][j];
-                    if (i_min == 0 || dij < d_min) {
-                        i_min = i;
-                        j_min = j;
-                        d_min = dij;
+            //Initialise d
+            //Compute the closest values for each taxa.
+            for (int i = 1; i <= ntax; i++) {
+                for (int j = i + 1; j <= ntax; j++) {
+                    //d[i][j] = d[j][i] = (distances.get(i, j) + distances.get(j, i)) / 2.0;
+                    double sum = distances.get(i, j) + distances.get(j, i);
+                    if (sum == distances.get(i, j) || sum == distances.get(j, i)) {
+                        d[i][j] = d[j][i] = sum;
+                    } else {
+                        d[i][j] = d[j][i] = sum / 2.0;
                     }
                 }
-
             }
 
+            int steps = 0;
+            for (int actual = ntax; actual > 2; actual--) {
 
-            double height = d_min / 2.0;
+                int i_min = 0, j_min = 0;
+                //Find closest pair.
+                double d_min = Double.MAX_VALUE;
+                for (int i = 1; i <= actual; i++) {
+                    for (int j = i + 1; j <= actual; j++) {
+                        double dij = d[i][j];
+                        if (i_min == 0 || dij < d_min) {
+                            i_min = i;
+                            j_min = j;
+                            d_min = dij;
+                        }
+                    }
 
-            Node v = tree.newNode();
-            Edge e = tree.newEdge(subtrees[i_min], v);
-            tree.setWeight(e, Math.max(height - heights[i_min], 0.0));
-            Edge f = tree.newEdge(subtrees[j_min], v);
-            tree.setWeight(f, Math.max(height - heights[j_min], 0.0));
-
-            subtrees[i_min] = v;
-            subtrees[j_min] = null;
-            heights[i_min] = height;
-
-
-            int size_i = sizes[i_min];
-            int size_j = sizes[j_min];
-            sizes[i_min] = size_i + size_j;
-
-            for (int k = 1; k <= ntax; k++) {
-                if ((k == i_min) || k == j_min) continue;
-                double dki = (d[k][i_min] * size_i + d[k][j_min] * size_j) / ((double) (size_i + size_j));
-                d[k][i_min] = d[i_min][k] = dki;
-            }
-
-            //Copy the top row of the matrix and arrays into the empty j_min row/column.
-            if (j_min < actual) {
-                for (int k = 1; k <= actual; k++) {
-                    d[j_min][k] = d[k][j_min] = d[actual][k];
                 }
-                d[j_min][j_min] = 0.0;
-                subtrees[j_min] = subtrees[actual];
-                sizes[j_min] = sizes[actual];
-                heights[j_min] = heights[actual];
+
+
+                double height = d_min / 2.0;
+
+                Node v = tree.newNode();
+                Edge e = tree.newEdge(subtrees[i_min], v);
+                tree.setWeight(e, Math.max(height - heights[i_min], 0.0));
+                Edge f = tree.newEdge(subtrees[j_min], v);
+                tree.setWeight(f, Math.max(height - heights[j_min], 0.0));
+
+                subtrees[i_min] = v;
+                subtrees[j_min] = null;
+                heights[i_min] = height;
+
+
+                int size_i = sizes[i_min];
+                int size_j = sizes[j_min];
+                sizes[i_min] = size_i + size_j;
+
+                for (int k = 1; k <= ntax; k++) {
+                    if ((k == i_min) || k == j_min) continue;
+                    double dki = (d[k][i_min] * size_i + d[k][j_min] * size_j) / ((double) (size_i + size_j));
+                    d[k][i_min] = d[i_min][k] = dki;
+                }
+
+                //Copy the top row of the matrix and arrays into the empty j_min row/column.
+                if (j_min < actual) {
+                    for (int k = 1; k <= actual; k++) {
+                        d[j_min][k] = d[k][j_min] = d[actual][k];
+                    }
+                    d[j_min][j_min] = 0.0;
+                    subtrees[j_min] = subtrees[actual];
+                    sizes[j_min] = sizes[actual];
+                    heights[j_min] = heights[actual];
+                }
+
+                steps += actual;
+                //progressListener.incrementProgress();
             }
 
-            steps += actual;
-            //progressListener.incrementProgress();
+            int sister = 2;
+            while (subtrees[sister] == null)
+                sister++;
+
+            //ToDo: fix phyloTree and get this to return a rooted tree.
+            //Edge e = tree.newEdge(subtrees[1], subtrees[sister]);
+            //tree.setWeight(e, d[1][sister]);
+
+            // root ???
+            Node root = tree.newNode();
+            tree.setLabel(root, "root");
+            Edge left = tree.newEdge(root, subtrees[1]);
+            Edge right = tree.newEdge(root, subtrees[sister]);
+            tree.setWeight(left, d[1][sister] / 2.0);
+            tree.setWeight(right, d[1][sister] / 2.0);
+            tree.setRoot(root);
+        } catch (Exception ex) {
+            Basic.caught(ex);
+            System.err.println(ex);
         }
-
-        int sister = 2;
-        while (subtrees[sister] == null)
-            sister++;
-
-        //ToDo: fix phyloTree and get this to return a rooted tree.
-        //Edge e = tree.newEdge(subtrees[1], subtrees[sister]);
-        //tree.setWeight(e, d[1][sister]);
-
-        // root ???
-        Node root = tree.newNode();
-        tree.setLabel(root, "root");
-        Edge left = tree.newEdge(root, subtrees[1]);
-        Edge right = tree.newEdge(root, subtrees[sister]);
-        tree.setWeight(left,  d[1][sister]/2.0);
-        tree.setWeight(right,  d[1][sister]/2.0);
-        tree.setRoot(root);
 
         return tree;
     }
