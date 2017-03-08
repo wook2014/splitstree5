1e3b8adaef78692b779c76602dd3a5631e650c8e
Daria <daria.elena.evseeva@gmail.com>
2017-03-08 22:38:00 +0100

    delete todo

--- src/splitstree5/core/algorithms/characters2distances/SequenceBasedDistance.java
+++ src/splitstree5/core/algorithms/characters2distances/SequenceBasedDistance.java
@@ -8,8 +8,6 @@ import splitstree5.core.datablocks.DistancesBlock;
 
 public abstract class SequenceBasedDistance extends Algorithm<CharactersBlock, DistancesBlock> {
 
-    // todo : implements IFromChararacters, IToDistances for all extended classes
-
     abstract public double getOptionPInvar();
 
     abstract public void setOptionPInvar(double pinvar);
@@ -17,7 +15,5 @@ public abstract class SequenceBasedDistance extends Algorithm<CharactersBlock, D
     abstract public double getOptionGamma();
 
     abstract public void setOptionGamma(double gamma);
-
-    //abstract public Distances computeDist(Characters characters);
 
 }
