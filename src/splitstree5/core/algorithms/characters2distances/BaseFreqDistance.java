98aba776f5f668141e692af22cab6f37dbd8ae87
Daria <daria.elena.evseeva@gmail.com>
2017-04-23 21:51:49 +0200

    fixed BaseFreqDistance

--- src/splitstree5/core/algorithms/characters2distances/BaseFreqDistance.java
+++ src/splitstree5/core/algorithms/characters2distances/BaseFreqDistance.java
@@ -34,7 +34,7 @@ public class BaseFreqDistance extends Algorithm<CharactersBlock, DistancesBlock>
         for (int s = 1; s <= ntax; s++) {
             System.err.print(taxaBlock.getLabel(s) + "\t");
             double count = 0;
-            for (int i = 1; i <= charactersBlock.getNchar(); i++) {
+            for (int i = 1; i < charactersBlock.getNchar(); i++) {
                 int x = symbols.indexOf(charactersBlock.get(s, i));
                 if (x >= 0) {
                     double weight = charactersBlock.getCharacterWeight(i);
@@ -70,6 +70,58 @@ public class BaseFreqDistance extends Algorithm<CharactersBlock, DistancesBlock>
             progressListener.incrementProgress();
         }
         progressListener.close();
+
+        /*String symbols = charactersBlock.getSymbols();
+        int nstates = symbols.length();
+
+        int ntax = taxaBlock.getNtax();
+        distancesBlock.setNtax(ntax);
+
+        progressListener.setTasks(TASK, "Init.");
+        progressListener.setMaximum(ntax);
+
+        double[][] baseFreqs = new double[ntax + 1][nstates];
+        System.err.println("Base Frequencies");
+
+        for (int s = 1; s <= ntax; s++) {
+            System.err.print(taxaBlock.getLabel(s) + "\t");
+            double count = 0;
+            for (int i = 1; i <= charactersBlock.getNchar(); i++) {
+                int x = symbols.indexOf(charactersBlock.get(s, i));
+                if (x >= 0) {
+                    double weight = charactersBlock.getCharacterWeight(i);
+                    baseFreqs[s][x] += weight;
+                    count += weight;
+                }
+            }
+
+            for (int x = 0; x < nstates; x++) {
+                baseFreqs[s][x] /= count;
+                System.err.print("" + baseFreqs[s][x] + "\t");
+            }
+            System.err.println("");
+        }
+
+
+        for (int s = 1; s <= ntax; s++) {
+            for (int t = s + 1; t <= ntax; t++) {
+                double p = 0.0;
+                for (int i = 0; i < nstates; i++) {
+                    double pi_i = baseFreqs[s][i];
+                    double pihat_i = baseFreqs[t][i];
+                    //  if (pi_i>0.0 && pihat_i>0.0)
+                    //    p+=pi_i * Math.log(pi_i/pihat_i) + pihat_i * Math.log(pihat_i/pi_i);
+                    p += Math.abs(pi_i - pihat_i);
+                }
+
+                distancesBlock.set(s, t, p);
+                distancesBlock.set(t, s, p);
+                //System.out.print(p+" ");
+            }
+            //System.out.println();
+            progressListener.incrementProgress();
+        }
+        progressListener.close();*/
     }
 
     // GETTER AND SETTER
--- src/splitstree5/core/algorithms/characters2distances/BaseFreqDistanceTest.java
+++ src/splitstree5/core/algorithms/characters2distances/BaseFreqDistanceTest.java
@@ -26,6 +26,8 @@ public class BaseFreqDistanceTest {
     @Test
     public void compute() throws Exception {
 
+        // todo try with weights
+
         String inputFile = "test//characters//mini.nex";
         ProgressListener pl = new ProgressPercentage();
         TaxaBlock taxaBlock = new TaxaBlock();
@@ -51,40 +53,41 @@ public class BaseFreqDistanceTest {
                 System.out.print(distancesBlock.get(i+1,j+1)+" ");
             }
             System.out.println();
-            assertArrayEquals(distancesFromSplitsTree4.getDistances()[i], distancesBlock.getDistances()[i], 0.0);
+            assertArrayEquals(distancesFromSplitsTree4.getDistances()[i], distancesBlock.getDistances()[i], 0.0000001);
         }
 
         // TEST 1
-        /*String inputFile = "test//characters//algae_rna_interleave.nex";
-        ProgressListener pl = new ProgressPercentage();
-        TaxaBlock taxaBlock = new TaxaBlock();
-        CharactersBlock charactersBlock = new CharactersBlock();
+        String inputFile1 = "test//characters//algae_rna_interleave.nex";
+        ProgressListener pl1 = new ProgressPercentage();
+        TaxaBlock taxaBlock1 = new TaxaBlock();
+        CharactersBlock charactersBlock1 = new CharactersBlock();
 
-        CharactersNexusFormat format = new CharactersNexusFormat();
-        List<String> taxonNames = CharactersNexusIO.parse(new NexusStreamParser(new FileReader(inputFile)), taxaBlock, charactersBlock, format);
-        taxaBlock.addTaxaByNames(taxonNames);
-        DistancesBlock distancesBlock = new DistancesBlock();
+        CharactersNexusFormat format1 = new CharactersNexusFormat();
+        List<String> taxonNames1 = CharactersNexusIO.parse(new NexusStreamParser(new FileReader(inputFile1)),
+                taxaBlock1, charactersBlock1, format1);
+        taxaBlock1.addTaxaByNames(taxonNames1);
+        DistancesBlock distancesBlock1 = new DistancesBlock();
 
-        baseFreqDistance.compute(pl, taxaBlock, charactersBlock, distancesBlock);
+        baseFreqDistance.compute(pl1, taxaBlock1, charactersBlock1, distancesBlock1);
 
-        final TaxaBlock taxaFromSplitsTree4 = new TaxaBlock();
-        final DistancesBlock distancesFromSplitsTree4 = new DistancesBlock();
-        taxaFromSplitsTree4.addTaxaByNames
+        final TaxaBlock taxaFromSplitsTree41 = new TaxaBlock();
+        final DistancesBlock distancesFromSplitsTree41 = new DistancesBlock();
+        taxaFromSplitsTree41.addTaxaByNames
                 (DistancesNexusIO.parse(new NexusStreamParser(new FileReader("test//distances//algaeBaseFreq.nex")),
-                        taxaFromSplitsTree4, distancesFromSplitsTree4, null));
+                        taxaFromSplitsTree41, distancesFromSplitsTree41, null));
 
 
         System.out.println("MATRIX");
-        for(int i = 0; i<distancesBlock.getDistances().length; i++){
-            for(int j = 0; j < distancesBlock.getDistances()[i].length; j++){
-                System.out.print(distancesBlock.get(i+1,j+1)+" ");
-            }
-            System.out.println();
-            assertArrayEquals(distancesFromSplitsTree4.getDistances()[i], distancesBlock.getDistances()[i], 0.001);
-        }*/
+        for(int i = 0; i<distancesBlock1.getDistances().length; i++){
+            //for(int j = 0; j < distancesBlock1.getDistances()[i].length; j++){
+              //  System.out.print(distancesBlock1.get(i+1,j+1)+" ");
+            //}
+            //System.out.println();
+            assertArrayEquals(distancesFromSplitsTree41.getDistances()[i], distancesBlock1.getDistances()[i], 0.0000001);
+        }
 
         // TEST 2
-        /*String inputFile2 = "test//characters//myosin_aa.nex";
+        String inputFile2 = "test//characters//myosin_aa.nex";
         ProgressListener pl2 = new ProgressPercentage();
         TaxaBlock taxaBlock2 = new TaxaBlock();
         CharactersBlock charactersBlock2 = new CharactersBlock();
@@ -104,8 +107,8 @@ public class BaseFreqDistanceTest {
 
 
         for(int i = 0; i<distancesBlock2.getDistances().length; i++){
-            assertArrayEquals(distancesFromSplitsTree4_2.getDistances()[i], distancesBlock2.getDistances()[i], 0.01);
-        }*/
+            assertArrayEquals(distancesFromSplitsTree4_2.getDistances()[i], distancesBlock2.getDistances()[i], 0.0000001);
+        }
 
     }
 
