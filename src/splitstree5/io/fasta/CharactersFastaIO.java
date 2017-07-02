a3efa08c2a7cfa393b06c77c83e583d437b529cd
Daria <daria.elena.evseeva@gmail.com>
2017-07-02 19:51:45 +0200

    working on FastA parser

--- src/splitstree5/io/fasta/CharactersFastaIO.java
+++ src/splitstree5/io/fasta/CharactersFastaIO.java
@@ -2,11 +2,13 @@ package splitstree5.io.fasta;
 
 import splitstree5.core.datablocks.CharactersBlock;
 import splitstree5.core.datablocks.TaxaBlock;
+import splitstree5.core.datablocks.characters.CharactersType;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Daria on 01.07.2017.
@@ -14,6 +16,11 @@ import java.util.ArrayList;
 public class CharactersFastaIO {
 
     public enum ID {ncbi, gb, emb, dbj, pir, prf, sp, pdb, pat, bbs, gnl, ref, lcl, unknown}
+    public final String[] extensions = {"fasta", "fas", "fa", "seq", "fsa"};
+
+    private static char gap = '-';
+    private static char missing = '?';
+    private static char matchChar='.';
 
     public static void parse(String inputFile, TaxaBlock taxa, CharactersBlock characters) throws IOException {
 
@@ -54,6 +61,7 @@ public class CharactersFastaIO {
             }
         }
         matrix.add(sequence);
+        if(nchar!=sequenceLength) throw new IOException("Sequences must be the same length");
         in.close();
 
         System.err.println("ntax: "+ntax+" nchar: "+nchar);
@@ -72,10 +80,74 @@ public class CharactersFastaIO {
 
     private static void readMatrix(ArrayList<String> matrix, CharactersBlock characters){
         // todo check if valid and set parameters here
+
+        String foundSymbols = "";
         for(int i = 1; i<=characters.getNtax(); i++){
             for(int j = 1; j<=characters.getNchar(); j++){
+                char symbol = Character.toLowerCase(matrix.get(i-1).charAt(j-1));
+                if(foundSymbols.indexOf(symbol) == -1) foundSymbols+=symbol;
                 characters.set(i, j, matrix.get(i-1).charAt(j-1));
             }
         }
+
+        foundSymbols = foundSymbols.replace(getStringGap(), "");
+        foundSymbols = foundSymbols.replace(getStringMissing(), "");
+        foundSymbols = foundSymbols.replace(getStringMatchChar(), "");
+        // sort found symbols
+        char[] chars = foundSymbols.toCharArray();
+        Arrays.sort(chars);
+        String sorted = new String(chars);
+
+        switch (sorted) {
+            case "01": characters.setDataType(CharactersType.standard);
+                break;
+            case "acgt": characters.setDataType(CharactersType.DNA);
+                break;
+            case "acgu": characters.setDataType(CharactersType.RNA);
+                break;
+            case "acdefghiklmnpqrstvwyz": characters.setDataType(CharactersType.protein);
+                break;
+            default: characters.setDataType(CharactersType.unknown);
+                break;
+        }
+        System.err.println("symbols: "+foundSymbols);
+    }
+
+    // GETTER AND SETTER
+
+    public static char getGap(){
+        return gap;
+    }
+
+    public static String getStringGap(){
+        return gap+"";
+    }
+
+    public static void setGap(char newGap){
+        gap = newGap;
+    }
+
+    public static char getMissing(){
+        return missing;
+    }
+
+    public static String getStringMissing(){
+        return missing+"";
+    }
+
+    public static void setMissing(char newMissing){
+        missing = newMissing;
+    }
+
+    public static char getMatchChar(){
+        return matchChar;
+    }
+
+    public static String getStringMatchChar(){
+        return matchChar+"";
+    }
+
+    public static void setMatchChar(char newMatchChar){
+        matchChar = newMatchChar;
     }
 }
