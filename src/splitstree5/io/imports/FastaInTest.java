17db395ed79672aff23137829058fbdd0b0fd505
Daria <daria.elena.evseeva@gmail.com>
2018-01-31 22:30:50 +0100

    fixed some bugs in FastA exports

--- src/splitstree5/io/imports/FastaInTest.java
+++ src/splitstree5/io/imports/FastaInTest.java
@@ -1,5 +1,6 @@
 package splitstree5.io.imports;
 
+import jloda.util.Basic;
 import jloda.util.ProgressPercentage;
 import org.junit.Test;
 import splitstree5.core.datablocks.CharactersBlock;
@@ -7,7 +8,13 @@ import splitstree5.core.datablocks.TaxaBlock;
 import splitstree5.io.nexus.CharactersNexusIO;
 import splitstree5.io.nexus.TaxaNexusIO;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Daria Evseeva,01.07.2017.
@@ -17,12 +24,12 @@ public class FastaInTest {
     private FastaIn fastaIn = new FastaIn();
 
     @Test
-    public void parse() throws Exception {
+    public void parseI() throws Exception {
 
         TaxaBlock taxaBlock = new TaxaBlock();
         CharactersBlock charactersBlock = new CharactersBlock();
         try (ProgressPercentage progress = new ProgressPercentage("Test")) {
-            fastaIn.parse(progress, "test/notNexusFiles/fasta/smallTest.fasta", taxaBlock, charactersBlock, null);
+            fastaIn.parse(progress, "test/notNexusFiles/smallTest.fasta", taxaBlock, charactersBlock, null);
         }
 
         // printing
@@ -34,6 +41,43 @@ public class FastaInTest {
 
         System.err.println("Ambiguous : " + charactersBlock.isHasAmbiguousStates());
 
+    }
+
+    @Test
+    public void parseII() throws Exception {
+
+        TaxaBlock taxaBlock = new TaxaBlock();
+        CharactersBlock charactersBlock = new CharactersBlock();
+        try (ProgressPercentage progress = new ProgressPercentage("Test")) {
+            fastaIn.parse(progress, "test/notNexusFiles/algae.fasta", taxaBlock, charactersBlock, null);
+        }
+
+        // printing
+        final StringWriter w = new StringWriter();
+        w.write("#nexus\n");
+        TaxaNexusIO.write(w, taxaBlock);
+        CharactersNexusIO.write(w, taxaBlock, charactersBlock, null);
+        System.err.println(w.toString());
+
+        System.err.println("Ambiguous : " + charactersBlock.isHasAmbiguousStates());
+
+    }
+
+    @Test
+    public void isApplicable() throws IOException {
+        ArrayList<String> applicableFiles = new ArrayList<>();
+
+        File directory = new File("test/notNexusFiles");
+        File[] directoryListing = directory.listFiles();
+        if (directoryListing != null) {
+            for (File file : directoryListing) {
+                if (fastaIn.isApplicable(file.getPath()))
+                    applicableFiles.add(Basic.getFileNameWithoutPath(file.getName()));
+            }
+        }
+        System.err.println(applicableFiles);
+        assertEquals(applicableFiles, Arrays.asList("algae.fasta", "algae_splits.fasta",
+                "ncbi.fasta", "smallTest.fasta", "trees49_splits.fasta"));
     }
 
 }
