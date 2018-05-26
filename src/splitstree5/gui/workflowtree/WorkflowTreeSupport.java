f9dd41f15f816ba14d71afe0aa97627b750d8230
Daniel <Daniel.Huson@uni-tuebingen.de>
2018-05-26 10:54:50 +0200

    fixed rerooting bugs, added duplication to workflow tree view

--- src/splitstree5/gui/workflowtree/WorkflowTreeSupport.java
+++ src/splitstree5/gui/workflowtree/WorkflowTreeSupport.java
@@ -40,8 +40,6 @@ public class WorkflowTreeSupport {
         treeView.setOnMouseEntered((e) -> treeView.requestFocus());
         treeView.setOnMouseExited((e) -> document.getMainWindow().getMainWindowController().getMainTabPane().requestFocus());
 
-        treeView.focusedProperty().addListener((c, o, n) -> System.err.println("Focus: " + n));
-
         workflow.getTopologyChanged().addListener((c, o, n) -> {
             Platform.runLater(() -> {
                 treeView.getRoot().getChildren().clear();
@@ -95,7 +93,6 @@ public class WorkflowTreeSupport {
                 addToTreeRec(document, treeItem, child);
             else
                 addToTreeRec(document, childItem, child);
-
         }
     }
 }
