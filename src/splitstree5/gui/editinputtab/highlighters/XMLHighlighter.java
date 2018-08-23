f088a0181798de6dfe60bd76b28724b2a398560d
Daria <daria.elena.evseeva@gmail.com>
2018-08-23 17:23:20 +0200

    added namespaces to the XMLHighlighter

--- src/splitstree5/gui/editinputtab/highlighters/XMLHighlighter.java
+++ src/splitstree5/gui/editinputtab/highlighters/XMLHighlighter.java
@@ -10,18 +10,25 @@ import java.util.regex.Pattern;
 
 public class XMLHighlighter implements Highlighter {
 
-    private static final Pattern XML_TAG = Pattern.compile("(?<ELEMENT>(</?\\h*)(\\w+)([^<>]*)(\\h*/?>))"
+    private static final String STRING_PATTERN = "\"([^\"\\\\]|\\\\.)*\"";
+    private static final String NAMESPACE_PATTERN = "\\b\\s+:";
+
+    private static final Pattern XML_TAG = Pattern.compile("(?<ELEMENT>(</?\\h*)(\\w+[:])?(\\w+)([^<>]*)(\\h*/?>))"
             +"|(?<COMMENT><!--[^<>]+-->)");
 
-    private static final Pattern ATTRIBUTES = Pattern.compile("(\\w+\\h*)(=)(\\h*\"[^\"]+\")");
+    private static final Pattern ATTRIBUTES = Pattern.compile("(\\w+[:])?(\\w+\\h*)(=)(\\h*\"[^\"]+\")");
 
     private static final int GROUP_OPEN_BRACKET = 2;
-    private static final int GROUP_ELEMENT_NAME = 3;
-    private static final int GROUP_ATTRIBUTES_SECTION = 4;
-    private static final int GROUP_CLOSE_BRACKET = 5;
-    private static final int GROUP_ATTRIBUTE_NAME = 1;
-    private static final int GROUP_EQUAL_SYMBOL = 2;
-    private static final int GROUP_ATTRIBUTE_VALUE = 3;
+    private static final int GROUP_ELEMENT_NAMESPACE = 3;
+    private static final int GROUP_ELEMENT_NAME = 4;
+    private static final int GROUP_ATTRIBUTES_SECTION = 5;
+    private static final int GROUP_CLOSE_BRACKET = 6;
+
+    // Attributes
+    private static final int GROUP_ATTRIBUTE_NAMESPACE = 1;
+    private static final int GROUP_ATTRIBUTE_NAME = 2;
+    private static final int GROUP_EQUAL_SYMBOL = 3;
+    private static final int GROUP_ATTRIBUTE_VALUE = 4;
 
 
     @Override
@@ -35,14 +42,25 @@ public class XMLHighlighter implements Highlighter {
             if(matcher.group("COMMENT") != null) {
                 spansBuilder.add(Collections.singleton("xml-comment"), matcher.end() - matcher.start());
             }
+            /*if(matcher.group("STRING") != null) {
+                spansBuilder.add(Collections.singleton("fasta"), matcher.end() - matcher.start());
+            }*/
             else {
                 if(matcher.group("ELEMENT") != null) {
                     String attributesText = matcher.group(GROUP_ATTRIBUTES_SECTION);
 
                     spansBuilder.add(Collections.singleton("xml-tagmark"),
                             matcher.end(GROUP_OPEN_BRACKET) - matcher.start(GROUP_OPEN_BRACKET));
-                    spansBuilder.add(Collections.singleton("xml-anytag"),
+
+                    if (matcher.group(GROUP_ELEMENT_NAMESPACE) != null) {
+                        spansBuilder.add(Collections.singleton("xml-namespace"),
+                                matcher.end(GROUP_ELEMENT_NAMESPACE) - matcher.end(GROUP_OPEN_BRACKET));
+                        spansBuilder.add(Collections.singleton("xml-anytag"),
+                                matcher.end(GROUP_ELEMENT_NAME) - matcher.end(GROUP_ELEMENT_NAMESPACE));
+                    } else {
+                        spansBuilder.add(Collections.singleton("xml-anytag"),
                             matcher.end(GROUP_ELEMENT_NAME) - matcher.end(GROUP_OPEN_BRACKET));
+                    }
 
                     if(!attributesText.isEmpty()) {
 
@@ -52,8 +70,17 @@ public class XMLHighlighter implements Highlighter {
                         while(amatcher.find()) {
                             spansBuilder.add(Collections.emptyList(),
                                     amatcher.start() - lastKwEnd);
-                            spansBuilder.add(Collections.singleton("xml-attribute"),
-                                    amatcher.end(GROUP_ATTRIBUTE_NAME) - amatcher.start(GROUP_ATTRIBUTE_NAME));
+
+                            if (amatcher.group(GROUP_ATTRIBUTE_NAMESPACE) == null)
+                                spansBuilder.add(Collections.singleton("xml-attribute"),
+                                        amatcher.end(GROUP_ATTRIBUTE_NAME) - amatcher.start(GROUP_ATTRIBUTE_NAME));
+                            else {
+                                spansBuilder.add(Collections.singleton("xml-namespace"),
+                                        amatcher.end(GROUP_ATTRIBUTE_NAMESPACE) - amatcher.start(GROUP_ATTRIBUTE_NAMESPACE));
+                                spansBuilder.add(Collections.singleton("xml-attribute"),
+                                        amatcher.end(GROUP_ATTRIBUTE_NAME) - amatcher.end(GROUP_ATTRIBUTE_NAMESPACE));
+                            }
+
                             spansBuilder.add(Collections.singleton("xml-tagmark"),
                                     amatcher.end(GROUP_EQUAL_SYMBOL) - amatcher.end(GROUP_ATTRIBUTE_NAME));
                             spansBuilder.add(Collections.singleton("xml-avalue"),
