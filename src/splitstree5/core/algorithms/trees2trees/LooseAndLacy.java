699846809e4d9e0bf53701f332ba865b739e1c50
Daniel <daniel.huson@uni-tuebingen.de>
2022-01-21 09:31:28 +0100

    updated copyright

--- src/splitstree5/core/algorithms/trees2trees/LooseAndLacy.java
+++ src/splitstree5/core/algorithms/trees2trees/LooseAndLacy.java
@@ -1,21 +1,20 @@
 /*
- * LooseAndLacy.java Copyright (C) 2021. Daniel H. Huson
+ * LooseAndLacy.java Copyright (C) 2022 Daniel H. Huson
  *
- * (Some code written by other authors, as named in code.)
+ * (Some files contain contributions from other authors, who are then mentioned separately.)
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package splitstree5.core.algorithms.trees2trees;
@@ -25,8 +24,8 @@ import jloda.graph.Edge;
 import jloda.graph.Node;
 import jloda.graph.NodeArray;
 import jloda.phylo.PhyloTree;
-import jloda.util.progress.ProgressListener;
 import jloda.util.StringUtils;
+import jloda.util.progress.ProgressListener;
 import splitstree5.core.algorithms.Algorithm;
 import splitstree5.core.algorithms.interfaces.IFromTrees;
 import splitstree5.core.algorithms.interfaces.IToTrees;
--- src/splitstree5/tools/ExportWorkflow.java
+++ src/splitstree5/tools/ExportWorkflow.java
@@ -1,21 +1,20 @@
 /*
- * ExportWorkflow.java Copyright (C) 2021. Daniel H. Huson
+ * ExportWorkflow.java Copyright (C) 2022 Daniel H. Huson
  *
- * (Some code written by other authors, as named in code.)
+ * (Some files contain contributions from other authors, who are then mentioned separately.)
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package splitstree5.tools;
@@ -27,7 +26,10 @@ import jloda.fx.util.ArgsOptions;
 import jloda.fx.util.ProgramExecutorService;
 import jloda.fx.util.ResourceManagerFX;
 import jloda.fx.window.NotificationManager;
-import jloda.util.*;
+import jloda.util.Basic;
+import jloda.util.FileUtils;
+import jloda.util.PeakMemoryUsageMonitor;
+import jloda.util.ProgramProperties;
 import jloda.util.progress.ProgressListener;
 import jloda.util.progress.ProgressPercentage;
 import splitstree5.core.datablocks.TaxaBlock;
